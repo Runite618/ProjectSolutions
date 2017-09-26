de2a508289aaa2b986c5abc315a63ef26d42c9f0
Matthew <matthew_mo@hotmail.co.uk>
2017-09-26 12:01:52 +0100

    Added new solution: DecimalToBinary

--- ChangeReturnProgram/src/ChangeReturn.java
+++ ChangeReturnProgram/src/ChangeReturn.java
@@ -5,9 +5,6 @@ import java.io.InputStreamReader;
 public class ChangeReturn {
     public static void main(String [] args) {
         int cost = 0, amountPaid = 0;
-        int fiver = 5;
-        int tenner = 10;
-        int twenty = 20;
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         System.out.println("How much does the item cost?");
         try {
@@ -24,6 +21,25 @@ public class ChangeReturn {
         if(amountPaid >= cost) {
             int change = amountPaid - cost;
             System.out.printf("The amount of change you shall receive is %d%n", change);
+            System.out.println("In terms of fivers, tenners and twenties you shall receive: ");
+            int twentyDivider = Math.round(change / 20);
+            int twentyRemainder = change % 20;
+            int tennerDivider = Math.round(twentyRemainder / 10);
+            int tennerRemainder = twentyRemainder % 10;
+            int fiverDivider = Math.round(tennerRemainder / 5);
+            int fiverRemainder = tennerRemainder % 5;
+            if(twentyDivider >= 1) {
+                System.out.printf("Twenties: %d%n", twentyDivider);
+            }
+            if(tennerDivider >= 1) {
+                System.out.printf("Tenners: %d%n", tennerDivider);
+            }
+            if(fiverDivider >= 1) {
+                System.out.printf("Fivers: %d%n", fiverDivider);
+            }
+            if(fiverRemainder >= 1){
+                System.out.printf("Pounds: %d%n", fiverRemainder);
+            }
         }
         else {
             System.out.println("Error! not enough money given to cover the cost of the item!");
