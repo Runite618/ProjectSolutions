55e57eeaca931b4eb852250b31f968fe736ebcdb
Matthew <matthew_mo@hotmail.co.uk>
2017-09-26 12:51:22 +0100

    decimalToBinary method and associated components complete

---
+++ DecimalToBinary/src/DecimalToBinary.java
@@ -0,0 +1,42 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class DecimalToBinary {
+    private ArrayList<Integer> intList;
+    private Scanner scanner;
+    private int remainder;
+
+    public DecimalToBinary() {
+        intList = new ArrayList<Integer>();
+    }
+
+    public void decimalToBinary(int decimal) {
+        while(decimal != 0) {
+            remainder = decimal % 2;
+            intList.add(remainder);
+            decimal = decimal / 2;
+        }
+
+        Collections.reverse(intList);
+        for(int element : intList) {
+            System.out.print(element);
+        }
+    }
+
+    public Scanner getScanner() {
+        return scanner;
+    }
+
+    public void setScanner(Scanner scanner) {
+        this.scanner = scanner;
+    }
+
+    public static void main(String [] args) {
+        DecimalToBinary decimalToBinary = new DecimalToBinary();
+        System.out.println("Please type in a number you wish to convert to binary");
+        decimalToBinary.setScanner(new Scanner(System.in));
+        int decimal = Integer.parseInt(decimalToBinary.getScanner().nextLine());
+        decimalToBinary.decimalToBinary(decimal);
+    }
+}
